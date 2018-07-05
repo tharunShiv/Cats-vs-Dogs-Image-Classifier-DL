7ffd8f5c4e6ade7acbeefe5b7d949caaa00cfa3c
tharunShiv <tharun.rocky14@gmail.com>
2018-07-05 23:36:20 +0530

    Part 2 build done

--- cnn.py
+++ cnn.py
@@ -48,3 +48,59 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))
 # adam->stochastic gradient descent
 # loss>
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+
+# Part 2 - Fitting the CNN to the images
+# to prevent overfitting
+# a great result on training set, poor resut in test set
+
+
+'''  REfer Keras documentation  '''
+# refer notes about data augmentation.
+
+'''
+We r working with 10000 images as our data now,
+but for the best results we need lakhs of data
+
+how are we gona satisfy that?
+using either more data / data augmentation
+
+it will create many batches of images,
+creating random transformation of each images
+
+hence it generates moreee images than what exists today..
+
+also prevents overfitting
+'''
+
+'
+# this is only one of the kind of transformation
+# check image preprocessing docs of keras
+from keras.preprocessing.image import ImageDataGenerator
+# rescale, all values will be between 0 and 1
+# 0.2 means, how much we wanna apply them
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+# target size, since we have already chosen 64 64 in our input shape
+# batch size after which our weights will be updated
+# cats and dogs, so binary
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
